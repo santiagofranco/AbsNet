5b4c85a9f2fc8078328d3fbcac2fed07497a92c8
Santiago Franco García <santifg92@gmail.com>
2018-05-23 00:09:28 +0200

    Initial Commit

---
+++ AbsNet/AbsNet.h
@@ -0,0 +1,19 @@
+//
+//  AbsNet.h
+//  AbsNet
+//
+//  Created by Santiago Franco García on 23/5/18.
+//  Copyright © 2018 Santiago Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AbsNet.
+FOUNDATION_EXPORT double AbsNetVersionNumber;
+
+//! Project version string for AbsNet.
+FOUNDATION_EXPORT const unsigned char AbsNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbsNet/PublicHeader.h>
+
+
